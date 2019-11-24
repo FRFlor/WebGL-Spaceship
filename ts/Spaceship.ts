ffecb0b0571268eb70841ae23726b1df8851e22a
Felipe Flor <f.flor@vehikl.com>
2019-11-24 14:51:18 -0500

    Extract player spaceship into dedicated class

---
+++ ts/Spaceship.ts
@@ -0,0 +1,134 @@
+import {Mesh} from "babylonjs";
+import {Scene} from "babylonjs/scene";
+import * as BABYLON from "babylonjs";
+import {renderAxes} from "./helpers";
+import {IUserInput} from "../types";
+import {Material} from "babylonjs/Materials/material";
+import {StandardMaterial} from "babylonjs/Materials/standardMaterial";
+
+const stabilizationStepAngle: number = BABYLON.Tools.ToRadians(1);
+const maneuverStepAngle: number = BABYLON.Tools.ToRadians(3);
+const maxManeuverAngle: number = BABYLON.Tools.ToRadians(20);
+const maneuverSpeed: number = 0.3;
+const forwardSpeed: number = 1;
+
+
+export class Spaceship {
+    public wrapper: Mesh;
+    public hasCollided: boolean = false;
+
+    private scene: Scene;
+    private showAxis: boolean = true;
+    private userInputs: IUserInput;
+
+    private defaultMaterials: Material[];
+    private damagedMaterial: StandardMaterial;
+
+    constructor(scene: Scene, userInputs: IUserInput) {
+        const wireframe = new BABYLON.StandardMaterial("wireframeMaterial", scene);
+        wireframe.wireframe = true;
+
+        this.scene = scene;
+        this.userInputs = userInputs;
+
+        this.wrapper = BABYLON.MeshBuilder.CreateBox("spaceship", {height: 1, width: 4, depth: 5}, scene);
+        this.wrapper.material = wireframe;
+        this.wrapper.ellipsoid = new BABYLON.Vector3(4, 1, 3);
+        this.wrapper.checkCollisions = true;
+
+        if (this.showAxis) {
+            const localAxis = renderAxes(3, scene);
+            localAxis.parent = this.wrapper;
+        }
+
+        BABYLON.SceneLoader.ImportMesh("", "./resources/gltf/low_poly_space_ship/", "scene.gltf", this.scene, (model) => {
+            model[0].position.y -= 0.85;
+            model[0].parent = this.wrapper;
+
+            this.defaultMaterials = this.wrapper.getChildMeshes().map((mesh) => mesh.material);
+            this.damagedMaterial = new BABYLON.StandardMaterial("red", scene);
+            this.damagedMaterial.diffuseColor = BABYLON.Color3.Red();
+        });
+    }
+
+    update() {
+        this.handleUserInput();
+        this.stabilizeFlight();
+        this.moveForward();
+    }
+
+    private handleUserInput() {
+        if (this.userInputs["w"]) {
+            this.wrapper.moveWithCollisions(new BABYLON.Vector3(0, maneuverSpeed , 0));
+            if (this.wrapper.rotation.x >= -maxManeuverAngle) {
+                this.wrapper.addRotation(-maneuverStepAngle, 0, 0);
+            }
+        }
+
+        if (this.userInputs["s"]) {
+            this.wrapper.moveWithCollisions(new BABYLON.Vector3(0, -maneuverSpeed , 0));
+            if (this.wrapper.rotation.x <= maxManeuverAngle) {
+                this.wrapper.addRotation(maneuverStepAngle, 0, 0);
+            }
+        }
+
+        if (this.userInputs["a"]) {
+            this.wrapper.moveWithCollisions(new BABYLON.Vector3(-maneuverSpeed, 0 , 0));
+            if (this.wrapper.rotation.z <= maxManeuverAngle) {
+                this.wrapper.addRotation(0, 0, maneuverStepAngle);
+            }
+        }
+
+        if (this.userInputs["d"]) {
+            this.wrapper.moveWithCollisions(new BABYLON.Vector3(maneuverSpeed, 0 , 0));
+            if (this.wrapper.rotation.z >= -maxManeuverAngle) {
+                this.wrapper.addRotation(0, 0, -maneuverStepAngle);
+            }
+        }
+    }
+
+    private moveForward() {
+        const desiredZ = this.wrapper.position.z + forwardSpeed;
+        this.wrapper.moveWithCollisions(new BABYLON.Vector3(0, 0 , forwardSpeed));
+        const hasTriggeredCollision = !this.hasCollided && desiredZ - this.wrapper.position.z > forwardSpeed/2;
+
+        if (hasTriggeredCollision) {
+            this.hasCollided = true;
+            this.wrapper.getChildMeshes().forEach((mesh) => mesh.material = this.damagedMaterial);
+            document.getElementById('overlay').classList.remove('hidden');
+            setTimeout(() => {
+                this.wrapper.getChildMeshes().forEach((mesh, index) => mesh.material = this.defaultMaterials[index]);
+                document.getElementById('overlay').classList.add('hidden');
+                this.hasCollided = false;
+            }, 1000);
+        }
+    }
+
+    private stabilizeFlight() {
+        const rotationAxisByKeyboardInput = {
+            w: 'x',
+            s: 'x',
+            a: 'z',
+            d: 'z'
+        };
+
+        ['x','y','z'].forEach(axis => {
+            const isKeyPressed = Object.keys(rotationAxisByKeyboardInput).some( key => {
+                return rotationAxisByKeyboardInput[key] === axis && this.userInputs[key]
+            });
+
+            if (isKeyPressed) {
+                return;
+            }
+
+            if (this.wrapper.rotation[axis] !== 0) {
+                this.wrapper.rotation[axis] += this.wrapper.rotation[axis] > 0 ? -stabilizationStepAngle : stabilizationStepAngle;
+            }
+            if (this.wrapper.rotation[axis] >= -stabilizationStepAngle && this.wrapper.rotation[axis] <= stabilizationStepAngle) {
+                this.wrapper.rotation[axis] = 0;
+            }
+        });
+    }
+
+
+}
--- ts/app.ts
+++ ts/app.ts
@@ -1,6 +1,6 @@
 import * as BABYLON from "babylonjs";
 import "babylonjs-loaders";
-import {renderAxes} from "./helpers";
+import {Spaceship} from "./Spaceship";
 
 const canvas: HTMLCanvasElement = document.getElementById("renderCanvas") as HTMLCanvasElement;
 const engine = new BABYLON.Engine(canvas, true);
@@ -8,20 +8,23 @@ const engine = new BABYLON.Engine(canvas, true);
 const createScene = () => {
     const scene = new BABYLON.Scene(engine);
 
+    const inputMap = {};
+    scene.actionManager = new BABYLON.ActionManager(scene);
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function (evt) {
+        inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+    }));
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, function (evt) {
+        inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+    }));
+
     const light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
     const light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
-
-    const wireframe = new BABYLON.StandardMaterial("wireframeMaterial", scene);
-    wireframe.wireframe = true;
 
     const origin = BABYLON.MeshBuilder.CreateBox("origin", {size: 0.5}, scene);
     origin.position.set(0, 0, 0);
 
     const metal = new BABYLON.StandardMaterial("grass0", scene);
     metal.diffuseTexture = new BABYLON.Texture("resources/textures/metal.jpg", scene);
-
-    const red = new BABYLON.StandardMaterial("red", scene);
-    red.diffuseColor = BABYLON.Color3.Red();
 
     const bottomWall = BABYLON.MeshBuilder.CreateBox('bottomWall', {height: 10, width: 100, depth: 1000}, scene);
     const topWall = BABYLON.MeshBuilder.CreateBox('topWall', {height: 10, width: 100, depth: 1000}, scene);
@@ -50,103 +53,17 @@ const createScene = () => {
         box.checkCollisions = true;
     });
 
-    let spaceShip = BABYLON.MeshBuilder.CreateBox("spaceship", {height: 1, width: 4, depth: 5}, scene);
-    spaceShip.material = wireframe;
-    spaceShip.ellipsoid = new BABYLON.Vector3(4, 1, 3);
-    spaceShip.checkCollisions = true;
-    let local = renderAxes(3, scene);
-    local.parent = spaceShip;
+    let spaceShip = new Spaceship(scene, inputMap);
 
     // const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 7), scene);
-    const camera = new BABYLON.FollowCamera("Camera", new BABYLON.Vector3(0, 0, 5), scene);
+    const camera = new BABYLON.FollowCamera("Camera", new BABYLON.Vector3(0, 0, -10), scene, spaceShip.wrapper);
+    camera.rotationOffset = 180;
     // camera.attachControl(canvas, true);
-
-    BABYLON.SceneLoader.ImportMesh("", "./resources/gltf/low_poly_space_ship/", "scene.gltf", scene, (model) => {
-        model[0].position.y -= 0.85;
-        model[0].parent = spaceShip;
-        camera.lockedTarget = model[0];
-    });
-
-    var inputMap = {};
-    scene.actionManager = new BABYLON.ActionManager(scene);
-    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function (evt) {
-        inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
-    }));
-    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, function (evt) {
-        inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
-    }));
 
     let spaceShipCollided = false;
     // Game/Render loop
     scene.onBeforeRenderObservable.add(() => {
-        const stabilizationStepAngle: number = BABYLON.Tools.ToRadians(1);
-        const maneuverStepAngle: number = BABYLON.Tools.ToRadians(3);
-        const maxManeuverAngle: number = BABYLON.Tools.ToRadians(20);
-        const maneuverSpeed: number = 0.3;
-
-        if (inputMap["w"] || inputMap["ArrowUp"]) {
-            spaceShip.moveWithCollisions(new BABYLON.Vector3(0, maneuverSpeed , 0));
-            if (spaceShip.rotation.x >= -maxManeuverAngle) {
-                spaceShip.addRotation(-maneuverStepAngle, 0, 0);
-            }
-        }
-        if (inputMap["a"] || inputMap["ArrowLeft"]) {
-            spaceShip.moveWithCollisions(new BABYLON.Vector3(-maneuverSpeed, 0 , 0));
-            if (spaceShip.rotation.z <= maxManeuverAngle) {
-                spaceShip.addRotation(0, 0, maneuverStepAngle);
-            }
-        }
-        if (inputMap["s"] || inputMap["ArrowDown"]) {
-            spaceShip.moveWithCollisions(new BABYLON.Vector3(0, -maneuverSpeed , 0));
-            if (spaceShip.rotation.x <= maxManeuverAngle) {
-                spaceShip.addRotation(maneuverStepAngle, 0, 0);
-            }
-        }
-        if (inputMap["d"] || inputMap["ArrowRight"]) {
-            spaceShip.moveWithCollisions(new BABYLON.Vector3(maneuverSpeed, 0 , 0));
-            if (spaceShip.rotation.z >= -maxManeuverAngle) {
-                spaceShip.addRotation(0, 0, -maneuverStepAngle);
-            }
-        }
-
-        const rotationAxisByKeyboardInput = {
-            w: 'x',
-            s: 'x',
-            a: 'z',
-            d: 'z'
-        };
-
-        ['x','y','z'].forEach(axis => {
-            const isKeyPressed = Object.keys(rotationAxisByKeyboardInput).some( key => {
-                return rotationAxisByKeyboardInput[key] === axis && inputMap[key]
-            });
-            if (isKeyPressed) {
-                return;
-            }
-            if (spaceShip.rotation[axis] !== 0) {
-                spaceShip.rotation[axis] += spaceShip.rotation[axis] > 0 ? -stabilizationStepAngle : stabilizationStepAngle;
-            }
-            if (spaceShip.rotation[axis] >= -stabilizationStepAngle && spaceShip.rotation[axis] <= stabilizationStepAngle) {
-                spaceShip.rotation[axis] = 0;
-            }
-        });
-
-        const desiredZ = spaceShip.position.z + 1;
-        spaceShip.moveWithCollisions(new BABYLON.Vector3(0, 0 , 1));
-
-        const originalSpaceshipMaterials = spaceShip.getChildMeshes().map((mesh) => mesh.material);
-
-        if (!spaceShipCollided && spaceShip.position.z > 5 && desiredZ - spaceShip.position.z > 0.5) {
-            spaceShipCollided = true;
-            spaceShip.getChildMeshes().forEach((mesh) => mesh.material = red);
-            document.getElementById('overlay').classList.remove('hidden');
-            setTimeout(() => {
-                spaceShip.getChildMeshes().forEach((mesh, index) => mesh.material = originalSpaceshipMaterials[index]);
-                document.getElementById('overlay').classList.add('hidden');
-                spaceShipCollided = false;
-            }, 1000);
-            // alert('Oh, U DED!!');
-        }
+        spaceShip.update();
     });
 
     return scene;
