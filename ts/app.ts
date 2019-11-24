6fbfa8fb48e63318925d4f9dd09edcf0876a0ee6
Felipe Flor <f.flor@vehikl.com>
2019-11-23 20:47:21 -0500

    Load a spaceship

--- ts/app.ts
+++ ts/app.ts
@@ -1,19 +1,36 @@
-import * as BABYLON from 'babylonjs';
+import * as BABYLON from "babylonjs";
+import "babylonjs-loaders";
+import {renderAxes} from "./helpers";
 
-const canvas: HTMLCanvasElement = document.getElementById('renderCanvas') as HTMLCanvasElement;
+const canvas: HTMLCanvasElement = document.getElementById("renderCanvas") as HTMLCanvasElement;
 const engine = new BABYLON.Engine(canvas, true);
 
 const createScene = () => {
     const scene = new BABYLON.Scene(engine);
 
-    const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5),
-        scene);
+    const light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+    const light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+
+    const wireframe = new BABYLON.StandardMaterial("wireframeMaterial", scene);
+    wireframe.wireframe = true;
+
+    const origin = BABYLON.MeshBuilder.CreateBox("origin", {size: 0.5}, scene);
+    origin.position.set(0, 0, 0);
+
+    let spaceShip = BABYLON.MeshBuilder.CreateBox('spaceship', {height: 1, width: 4, depth: 5}, scene);
+    spaceShip.material = wireframe;
+    let local = renderAxes(3, scene);
+    local.parent = spaceShip;
+
+    // const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 7), scene);
+    const camera = new BABYLON.FollowCamera("Camera", new BABYLON.Vector3(0, 0, 5), scene);
     camera.attachControl(canvas, true);
 
-    const light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 0), scene);
-    const light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 1, -1), scene);
-
-    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {diameter: 2}, scene);
+    BABYLON.SceneLoader.ImportMesh("", "./resources/gltf/low_poly_space_ship/", "scene.gltf", scene, (model) => {
+        model[0].position.y -= 0.85;
+        model[0].parent = spaceShip;
+        camera.lockedTarget = model[0];
+    });
 
     return scene;
 };
@@ -21,9 +38,12 @@ const createScene = () => {
 const scene = createScene();
 
 engine.runRenderLoop(() => {
+    // scene.getMeshByName("spaceship").rotate(new BABYLON.Vector3(0, 0, 1), 0.01);
     scene.render();
 });
 
-window.addEventListener('resize', function() {
+window.addEventListener("resize", function () {
     engine.resize();
 });
+
+
---
+++ ts/helpers.ts
@@ -0,0 +1,30 @@
+import * as BABYLON from "babylonjs";
+
+export function renderAxes(size, scene) {
+    const local_axisX = BABYLON.Mesh.CreateLines("local_axisX", [
+        BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
+        new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+    ], scene);
+    local_axisX.color = new BABYLON.Color3(1, 0, 0);
+
+    const local_axisY = BABYLON.Mesh.CreateLines("local_axisY", [
+        BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+        new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+    ], scene);
+    local_axisY.color = new BABYLON.Color3(0, 1, 0);
+
+    const local_axisZ = BABYLON.Mesh.CreateLines("local_axisZ", [
+        BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0 , -0.05 * size, size * 0.95),
+        new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0, 0.05 * size, size * 0.95)
+    ], scene);
+    local_axisZ.color = new BABYLON.Color3(0, 0, 1);
+
+    const local_origin = BABYLON.MeshBuilder.CreateBox("local_origin", {size:1}, scene);
+    local_origin.isVisible = false;
+
+    local_axisX.parent = local_origin;
+    local_axisY.parent = local_origin;
+    local_axisZ.parent = local_origin;
+
+    return local_origin;
+}
